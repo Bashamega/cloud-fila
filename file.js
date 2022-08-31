2a8cdd30058b75ad4d7c1993e533cedcbb2eb528
Bashamega <110662505+Bashamega@users.noreply.github.com>
2022-08-31 09:44:18 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,10 @@
+
+function Relationships(){
+    window.open("https://bashamega.github.io/files/Relationships-between-different-plant-groups.pdf")
+}
+function rootfunctions(){
+    window.open("https://bashamega.github.io/files/Summary-of-root-functions.pdf")
+}
+function rootfunctionsbytype(){
+    window.open("https://bashamega.github.io/files/Summary-of-root-functions-by-type.pdf")
+}
---
+++ script.js
@@ -0,0 +1,17 @@
+function openpage(){
+    var x = document.getElementById("shearch").value;
+
+    if(x=== "home"){
+        window.open("https://bashamega.github.io/files/");
+    }
+    if(x=== "Relationships between different plants groups"){
+        window.open("https://bashamega.github.io/files/Relationships-between-different-plant-groups.pdf")
+    }
+    if(x=== "Summary of root functions"){
+        window.open("https://bashamega.github.io/files/Summary-of-root-functions.pdf")
+    }
+    if(x=== "Summary of root functions by type"){
+        window.open("https://bashamega.github.io/files/Summary-of-root-functions-by-type.pdf")
+    }
+    
+}
